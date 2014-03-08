be4c5bbc7ac4d952c90a6cd049f1687342b8dce2
Bradley Arsenault <genixpro@gmail.com>
2014-03-07 23:26:04 -0500

    another minor change to the vagrant bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ apt-get install -y python mercurial python-pip python-dev python-imaging g++
 
 apt-get install -y virtualbox-guest-additions 
 
-cd /vagrant/weatherapp
+cd /vagrant
 python setup.py develop
 
 pserve production.ini &
